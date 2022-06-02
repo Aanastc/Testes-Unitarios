facbf52a3cec6254b935d7e167cd754b7a42f161
Anastc <ananovos@hotmail.com>
2022-06-01 21:39:00 -0300

    mudacas

--- mdc.test.js
+++ mdc.test.js
@@ -1,13 +1,13 @@
 const {mdc, mdcLista} = require('./mdc.js');
 
 describe('mdc', () => {
-    it('fibonacci', () => {
+    it('mdc', () => {
         expect(mdc(160, 32, 26)).toEqual(160, 32, 26);
     })
 })
 
 describe('mdc', () => {
-    it('fibonacci', () => {
+    it('mdc', () => {
         expect(mdcLista(0)).toEqual(0);
     })
 })
